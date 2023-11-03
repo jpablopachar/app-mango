56b5340358f06fa23fc7459eeb7a580b9756400c
jpablopachar <jpablopachar1993@gmail.com>
2023-11-03 07:09:39 -0500

    Se documenta interfaces de IBaseService y ITokenProvider

--- client/Interfaces/IBaseService.cs
+++ client/Interfaces/IBaseService.cs
@@ -4,6 +4,10 @@ namespace client.Interfaces
 {
     public interface IBaseService
     {
+        /// <summary>Sends an asynchronous request with an optional bearer token.</summary>
+        /// <param name="RequestDto">Contains the request data to be sent.</param>
+        /// <param name="withBearer">Indicates whether to include a bearer token in the
+        /// request headers.</param>
         Task<ResponseDto> SendAsync(RequestDto requestDto, bool withBearer = true);
     }
 }
--- client/Interfaces/ITokenProvider.cs
+++ client/Interfaces/ITokenProvider.cs
@@ -2,8 +2,14 @@ namespace client.Interfaces
 {
     public interface ITokenProvider
     {
+        /// <summary>Sets a token value.</summary>
+        /// <param name="token">The token value that you want to set.</param>
         void SetToken(string token);
+
+        /// <summary>Returns a nullable string.</summary>
         string? GetToken();
+
+        /// <summary>Clears a token.</summary>
         void ClearToken();
     }
 }
