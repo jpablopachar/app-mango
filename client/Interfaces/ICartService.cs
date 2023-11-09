225e2e666f8f24a9eb0a81210715f92b1060b3f6
jpablopachar <jpablopachar1993@gmail.com>
2023-11-09 16:03:36 -0500

    Interfaz para servicio ICartService

---
+++ client/Interfaces/ICartService.cs
@@ -0,0 +1,13 @@
+using client.Models;
+
+namespace client.Interfaces
+{
+    public interface ICartService
+    {
+        Task<ResponseDto> GetCartByUserIdAsync(string userId);
+        Task<ResponseDto> UpsertCartAsync(CartDto cartDto);
+        Task<ResponseDto> RemoveFromCartAsync(int cartDetailsId);
+        Task<ResponseDto> ApplyCouponAsync(CartDto cartDto);
+        Task<ResponseDto> EmailCart(CartDto cartDto);
+    }
+}
