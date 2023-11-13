5cb8a741fc71e9af298ad0da668535b22402658a
jpablopachar <jpablopachar1993@gmail.com>
2023-11-13 17:14:30 -0500

    Implementación de mensajería

---
+++ services/email-service/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,15 @@
+namespace email_service.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        public Task Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
