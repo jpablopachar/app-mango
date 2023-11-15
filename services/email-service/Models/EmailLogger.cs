ed1ed2e537d49ee10de14db5bd74575a21912a51
jpablopachar <jpablopachar1993@gmail.com>
2023-11-15 09:33:01 -0500

    Primera migración en EmailService

--- services/email-service/Models/EmailLogger.cs
+++ services/email-service/Models/EmailLogger.cs
@@ -2,7 +2,7 @@ namespace email_service.Models
 {
     public class EmailLogger
     {
-        public int Int { get; set; }
+        public int Id { get; set; }
         public string? Email { get; set; }
         public string? Message { get; set; }
         public DateTime EmailSent { get; set; }
