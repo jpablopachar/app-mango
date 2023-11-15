fcc15a0b5d0a304bd8adf789661acf50a719095a
jpablopachar <jpablopachar1993@gmail.com>
2023-11-15 01:47:34 -0500

    Se agrega funcionalidad AzureServiceBus

--- services/email-service/Messaging/AzureServiceBusConsumer.cs
+++ services/email-service/Messaging/AzureServiceBusConsumer.cs
@@ -1,15 +1,93 @@
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using email_service.Dtos;
+using email_service.Services;
+using Newtonsoft.Json;
+
 namespace email_service.Messaging
 {
     public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
-        public Task Start()
+        private readonly string _serviceBusConnectionString;
+        private readonly string _emailCartQueue;
+        private readonly string _registerUserQueue;
+        private readonly IConfiguration _configuration;
+        private readonly EmailService _emailService;
+        private readonly ServiceBusProcessor _emailCartProcessor;
+        private readonly ServiceBusProcessor _registerUserProcessor;
+
+        public AzureServiceBusConsumer(IConfiguration configuration, EmailService emailService)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            _emailService = emailService;
+
+            _serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString")!;
+            _emailCartQueue = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue")!;
+            _registerUserQueue = _configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue")!;
+
+            var client = new ServiceBusClient(_serviceBusConnectionString);
+
+            _emailCartProcessor = client.CreateProcessor(_emailCartQueue);
+            _registerUserProcessor = client.CreateProcessor(_registerUserQueue);
         }
 
-        public Task Stop()
+        public async Task Start()
         {
-            throw new NotImplementedException();
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+
+            await _emailCartProcessor.StartProcessingAsync();
+
+            _registerUserProcessor.ProcessMessageAsync += OnUserRegisterRequestReceived;
+            _registerUserProcessor.ProcessErrorAsync += ErrorHandler;
+
+            await _registerUserProcessor.StartProcessingAsync();
+        }
+
+        public async Task Stop()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
+
+            await _registerUserProcessor.StopProcessingAsync();
+            await _registerUserProcessor.DisposeAsync();
+        }
+
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(body)!;
+
+            try
+            {
+                await _emailService.EmailCartAndLogAsync(cartDto);
+                await args.CompleteMessageAsync(message);
+            }
+            catch (Exception) { throw; }
+        }
+
+        private async Task OnUserRegisterRequestReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            string email = JsonConvert.DeserializeObject<string>(body)!;
+
+            try
+            {
+                await _emailService.RegisterUserEmailAndLogAsync(email);
+                await args.CompleteMessageAsync(message);
+            }
+            catch (Exception) { throw; }
+        }
+
+        private static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+
+            return Task.CompletedTask;
         }
     }
 }
