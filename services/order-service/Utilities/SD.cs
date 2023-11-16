a301fec260b62a2723eff5ab1ec73d1ba552b5d7
jpablopachar <jpablopachar1993@gmail.com>
2023-11-16 08:26:17 -0500

    Creación de Utilidades para OrderService

---
+++ services/order-service/Utilities/SD.cs
@@ -0,0 +1,14 @@
+namespace order_service.Utilities
+{
+    public class SD
+    {
+        public const string STATUS_PENDING = "Pending";
+        public const string STATUS_APPROVED = "Approved";
+        public const string STATUS_READY_FOR_PICKUP = "ReadyForPickup";
+        public const string STATUS_COMPLETED = "Completed";
+        public const string STATUS_REFUNDED = "Refunded";
+        public const string STATUS_CANCELLED = "Cancelled";
+        public const string ROLE_ADMIN = "ADMIN";
+        public const string ROLE_CUSTOMER = "CUSTOMER";
+    }
+}
