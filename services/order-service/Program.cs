c65d6078373f3aab9661cbbb2a2b7661b94c3f89
jpablopachar <jpablopachar1993@gmail.com>
2023-11-16 12:57:11 -0500

    Se agrega configuración inicial de Stripe

--- services/order-service/Program.cs
+++ services/order-service/Program.cs
@@ -71,6 +71,8 @@ app.UseSwaggerUI(c =>
     }
 });
 
+Stripe.StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
