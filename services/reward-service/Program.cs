a67589ded66f49fd7155739ce9c75917de45794b
jpablopachar <jpablopachar1993@gmail.com>
2023-11-24 15:50:45 -0500

    Configuración inicial para reward-service

--- services/reward-service/Program.cs
+++ services/reward-service/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using reward_service.Data;
+using reward_service.Extension;
+using reward_service.Interfaces;
+using reward_service.Messaging;
+using reward_service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<RewardDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection")));
+
+var optionBuilder = new DbContextOptionsBuilder<RewardDbContext>();
+
+optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"));
+
+builder.Services.AddSingleton(new RewardService(optionBuilder.Options));
+
+builder.Services.AddSingleton<IAzureServiceBusConsumer, AzureServiceBusConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,11 +26,16 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (!app.Environment.IsDevelopment())
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cart API");
+        c.RoutePrefix = string.Empty;
+    }
+});
 
 app.UseHttpsRedirection();
 
@@ -22,4 +43,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
+app.UseAzureServiceBusConsumer();
+
 app.Run();
+
+void ApplyMigration()
+{
+    using var scope = app.Services.CreateScope();
+    var _db = scope.ServiceProvider.GetRequiredService<RewardDbContext>();
+
+    if (_db.Database.GetPendingMigrations().Count() > 0)
+    {
+        _db.Database.Migrate();
+    }
+}
